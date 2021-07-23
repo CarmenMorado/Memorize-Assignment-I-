4b2ac6a88964923626fc7f6add6a0e1637792a39
Carmen Morado <cmora171@fiu.edu>
2021-07-23 16:40:18 -0400

    Initial Commit

---
+++ Memorize (Assignment I)/ContentView.swift
@@ -0,0 +1,111 @@
+//
+//  ContentView.swift
+//  Memorize (Assignment I)
+//
+//  Created by Carmen Morado on 7/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var vehicleEmojis = ["🚗","🚌","🚑","🚜","🛴","🚲","🚔","🛺","🚘","🚃","🚄","🚠","🚅","✈️","🚀","🛸","🚁","🛶","🚤","🚢","🛳","🚓","🚛","🛵"]
+    var foodEmojis = ["🍕", "🌭", "🍟", "🍔", "🥘", "🍝", "🍜", "🎂", "🍰", "🍦"]
+    var animalEmojis = ["🦄", "🦊", "🐥", "🦋", "🐬", "🐳", "🐞", "🐶", "🐱", "🐹", "🦉", "🐒"]
+    @State var emojis = ["🚗","🚌","🚑","🚜","🛴","🚲","🚔","🛺","🚘","🚃","🚄","🚠","🚅","✈️","🚀","🛸","🚁","🛶","🚤","🚢","🛳","🚓","🚛","🛵"]
+        @State var emojiCount: Int = 8
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                Spacer()
+                Text("Memorize!")
+                    .font(.largeTitle)
+                LazyVGrid(columns:[GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.blue)
+            HStack {
+                vehicleTheme
+                Spacer()
+                animalTheme
+                Spacer()
+                foodTheme
+            }
+            .padding(.horizontal)
+            .font(.largeTitle)
+        }
+        .padding(.horizontal)
+    }
+    
+    var vehicleTheme: some View {
+        Button(action: {
+            emojis = vehicleEmojis.shuffled()
+            emojiCount = 16
+        }, label: {
+            VStack{
+                Image(systemName: "car").font(.largeTitle)
+                   Text("Vehicles").font(.body)
+            }
+        })
+    }
+    
+    var animalTheme: some View {
+        Button(action: {
+            emojis = animalEmojis.shuffled()
+            emojiCount = 12
+        }, label: {
+            VStack{
+                Image(systemName: "ladybug").font(.largeTitle)
+                Text("Animals").font(.body)
+            }
+        })
+    }
+    
+    var foodTheme: some View {
+        Button(action: {
+            emojis = foodEmojis.shuffled()
+            emojiCount = 8
+        }, label: {
+            VStack{
+                Image(systemName: "cart").font(.largeTitle)
+                Text("Food").font(.body)
+            }
+        })
+    }
+}
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius:20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth:3)
+                Text(content).font(.largeTitle)
+                
+            }
+            
+            else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
+    }
+}
---
+++ Memorize (Assignment I)/Memorize__Assignment_I_App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize__Assignment_I_App.swift
+//  Memorize (Assignment I)
+//
+//  Created by Carmen Morado on 7/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Memorize__Assignment_I_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
